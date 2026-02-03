d07bb398524ca16e54abcbe4d0848bdc2eec8472
DylanAbrahams <143963818+DylanAbrahams@users.noreply.github.com>
2026-02-03 15:42:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Dit is een log");
+
+
+const button = document.querySelector('.background-button');
+
+let isDark = false;
+
+button.addEventListener('click', () => {
+if (isDark) {
+    document.body.style.backgroundColor = '#add8e6'; // light blue
+} else {
+    document.body.style.backgroundColor = '#0b3c5d'; // dark blue
+}
+
+isDark = !isDark;
+});
